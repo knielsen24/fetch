a51daae736db4783d33a8fb5ffa4f7a584ad31de
Kevin Nielsen <knielsen24@gmail.com>
2022-07-09 15:30:20 -0700

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import NavBar from "./Component/NavBar";
 import LandingPage from "./Component/LandingPage";
+import { Routes, Route, Links, useParams, useNavigate } from "react-router-dom";
+import SignIn from "./Component/SignIn";
 
 function App() {
-   return (
+
+	let {jobListingId} = useParams();
+	let navigate = useNavigate();
+
+	return (
       <>
          <NavBar />
-         <LandingPage />
+         <Routes>
+				<Route path="/" element={ <LandingPage /> } />
+				<Route path="/signin" element={ <SignIn /> } />
+				<Route path="/profile" />
+				<Route path="/findjobs/:jobListingId" />
+				<Route path="companyreviews" />
+			</Routes>
       </>
    );
 }
