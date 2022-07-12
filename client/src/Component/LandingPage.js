17c73e41f9add75042b28fb2410c18021210a9c5
Kevin Nielsen <knielsen24@gmail.com>
2022-07-12 09:44:18 -0700

    Feature

--- client/src/Component/LandingPage.js
+++ client/src/Component/LandingPage.js
@@ -1,15 +1,5 @@
 import React, { useState } from "react";
-import {
-   Paper,
-   FormControl,
-   
-   InputLabel,
-   Button,
-   Stack,
-   Box,
-   TextField,
-   Divider,
-} from "@mui/material";
+import { Button, Stack, Box, TextField, Divider } from "@mui/material";
 
 export default function LandingPage({ setUser }) {
    const newUser = {
@@ -78,87 +68,109 @@ export default function LandingPage({ setUser }) {
                src="https://i.imgur.com/PHXOSF9.png"
                alt="create an account"
             />
-            
-            <Box sx={{ width: 350, height: 525, backgroundColor: "white", borderRadius: "8px",}}>
-                  <Box display={'flex'} alignItems={'center'}  justifyContent={'center'} sx={{ color: 'white', background: 'rgba(0, 224, 255, 1)', width: 350, height: 50, borderTopLeftRadius: "8px", borderTopRightRadius: "8px", borderBottomLeftRadius: "0px", borderBottomRightRadius: "0px", marginBottom: 3}}>
-                     <h3>Create An Account</h3>
+
+            <Box
+               sx={{
+                  width: 350,
+                  height: 525,
+                  backgroundColor: "white",
+                  borderRadius: "8px",
+               }}
+            >
+               <Box
+                  display={"flex"}
+                  alignItems={"center"}
+                  justifyContent={"center"}
+                  sx={{
+                     color: "white",
+                     background: "rgba(0, 224, 255, 1)",
+                     width: 350,
+                     height: 50,
+                     borderTopLeftRadius: "8px",
+                     borderTopRightRadius: "8px",
+                     borderBottomLeftRadius: "0px",
+                     borderBottomRightRadius: "0px",
+                     marginBottom: 3,
+                  }}
+               >
+                  <h3>Create An Account</h3>
+               </Box>
+               <form onSubmit={handleSubmit} onChange={handleChange}>
+                  <Box sx={{ width: 300, margin: "auto" }}>
+                     <Stack
+                        spacing={2.5}
+                        alignItems={"center"}
+                        justifyContent={"center"}
+                     >
+                        <TextField
+                           size="small"
+                           fullWidth
+                           type="first_name"
+                           id="first_name"
+                           name="first_name"
+                           placeholder="First Name"
+                           value={signUpData.first_name}
+                        />
+                        <TextField
+                           size="small"
+                           fullWidth
+                           type="last_name"
+                           id="last_name"
+                           name="last_name"
+                           placeholder="Last Name"
+                           value={signUpData.last_name}
+                        />
+                        <TextField
+                           size="small"
+                           fullWidth
+                           type="email"
+                           id="email"
+                           name="email"
+                           placeholder="Email"
+                           value={signUpData.email}
+                        />
+                        <TextField
+                           size="small"
+                           fullWidth
+                           type="password"
+                           id="password"
+                           name="password"
+                           placeholder="Password"
+                           value={signUpData.password}
+                        />
+                        <TextField
+                           size="small"
+                           fullWidth
+                           type="password"
+                           id="password_confirmation"
+                           name="password_confirmation"
+                           placeholder="Confirm Password"
+                           value={signUpData.password_confirmation}
+                        />
+                        <Button
+                           disableElevation
+                           fullWidth
+                           variant="contained"
+                           size="small"
+                           type="submit"
+                        >
+                           Sign Up
+                        </Button>
+                        <h8> Already Have An Account?</h8>
+                        <Button
+                           fullWidth
+                           disableElevation
+                           variant="contained"
+                           size="small"
+                           type="click"
+                        >
+                           Sign In
+                        </Button>
+                     </Stack>
                   </Box>
-                     <form onSubmit={handleSubmit} onChange={handleChange}>
-                        <Box sx={{width: 300, margin:'auto'}}>
-                        <Stack spacing={2.5} alignItems={'center'} justifyContent={'center'} >
-                           <TextField
-                              size="small"
-                              fullWidth
-                                 type="first_name"
-                                 id="first_name"
-                                 name="first_name"
-                                 placeholder="First Name"
-                                 value={signUpData.first_name}
-                                 
-                              />
-                              <TextField
-                              size="small"
-                              fullWidth
-                                 type="last_name"
-                                 id="last_name"
-                                 name="last_name"
-                                 placeholder="Last Name"
-                                 value={signUpData.last_name}
-                              />
-                              <TextField
-                              size="small"
-                              fullWidth
-                                 type="email"
-                                 id="email"
-                                 name="email"
-                                 placeholder="Email"
-                                 value={signUpData.email}
-                              />
-                              <TextField
-                              size="small"
-                              fullWidth
-                                 type="password"
-                                 id="password"
-                                 name="password"
-                                 placeholder="Password"
-                                 value={signUpData.password}
-                              />
-                              <TextField
-                              size="small"
-                                 fullWidth
-                                 type="password"
-                                 id="password_confirmation"
-                                 name="password_confirmation"
-                                 placeholder="Confirm Password"
-                                 value={
-                                    signUpData.password_confirmation
-                                 }
-                              />
-                              <Button
-                              disableElevation
-                              fullWidth
-                                 variant="contained"
-                                 size="small"
-                                 type="submit"
-                              >
-                                 Sign Up
-                              </Button>
-                              <h8> Already Have An Account?</h8>
-                              <Button
-                              fullWidth
-                              disableElevation
-                                 variant="contained"
-                                 size="small"
-                                 type="click"
-                              >
-                                 Sign In
-                              </Button>
-                        </Stack>
-                        </Box>
-                     </form>
-                     
-                  </Box>
-          </Stack>
+               </form>
+            </Box>
+         </Stack>
       </>
    );
 }
--- client/src/Component/ProfileFeature.js
+++ client/src/Component/ProfileFeature.js
@@ -1,9 +1,68 @@
-import React from "react";
+import { Button, Stack, Box, TextField, Divider } from "@mui/material";
 
 function ProfileFeature({ user }) {
-   const { first_name, last_name, email, image_url, resume } = user;
+   // const { first_name, last_name, email, image_url, resume } = user;
 
-   return <div>{first_name + " " + last_name}</div>;
+   return (
+      <div>
+
+         <Box
+            sx={{
+               width: 350,
+               height: 525,
+               backgroundColor: "white",
+               borderRadius: "8px",
+            }}
+         >
+            <Box
+               display={"flex"}
+               alignItems={"center"}
+               justifyContent={"center"}
+               sx={{
+                  color: "white",
+                  background: "rgba(0, 224, 255, 1)",
+                  width: 350,
+                  height: 50,
+                  borderTopLeftRadius: "8px",
+                  borderTopRightRadius: "8px",
+                  borderBottomLeftRadius: "0px",
+                  borderBottomRightRadius: "0px",
+                  marginBottom: 3,
+               }}
+            >
+               <h3>Create An Account</h3>
+            </Box>
+               <Box sx={{ width: 300, margin: "auto" }}>
+                  <Stack
+                     spacing={2.5}
+                     alignItems={"center"}
+                     justifyContent={"center"}
+                  >
+                     <TextField />
+
+                     <Button
+                        disableElevation
+                        fullWidth
+                        variant="contained"
+                        size="small"
+                        type="submit"
+                     >
+                        Edit Profile
+                     </Button>
+                     <Button
+                        fullWidth
+                        disableElevation
+                        variant="contained"
+                        size="small"
+                        type="click"
+                     >
+                        Delete
+                     </Button>
+                  </Stack>
+               </Box>
+         </Box>
+      </div>
+   );
 }
 
 export default ProfileFeature;
