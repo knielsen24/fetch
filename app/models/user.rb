177acf627e8d3641e8f50d5ee325be5bc247cd1f
Kevin Nielsen <knielsen24@gmail.com>
2022-07-12 11:33:37 -0700

    validations comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
     has_many :companies, through: :reviews
     has_many :applied_to
     has_many :job_postings, through: :applied_to
+
+	#  validates :email, :first_name, :last_name, presence: true
+	#  validates :email, uniqueness: true
 end
