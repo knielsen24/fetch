6c2fd6e4050c8b4b69fb8f5a819cfa4b6bf558d2
Kevin Nielsen <knielsen24@gmail.com>
2022-07-14 12:50:41 -0700

    sss22sss

--- client/src/Component/AppliedToJobs.js
+++ client/src/Component/AppliedToJobs.js
@@ -3,17 +3,15 @@ import React, { useState, useEffect } from "react";
 export default function AppliedToJobs({ user }) {
    const [appliedJobs, setAppliedJobs] = useState([]);
 
-   // const renderProfileJobs = appliedJobs.map(application => {
-   //     return application.job_posting
-   // })
-
-   console.log(appliedJobs)
-
    useEffect(() => {
       fetch(`/users/${user.id}/applications`)
          .then((r) => r.json())
          .then((data) => setAppliedJobs(data));
    }, []);
+
+   const renderProfileJobs = appliedJobs.map((application) => {
+      return application.job_posting;
+   });
 
    return <div>AppliedToJobs</div>;
 }
