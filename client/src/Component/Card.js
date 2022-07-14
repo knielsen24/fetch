8dc662a42fe3924976ddde2160c7107e0beb2a2d
Christian <christianstringham@gmail.com>
2022-07-14 12:59:59 -0600

    profilestyled

--- client/src/Component/Card.js
+++ client/src/Component/Card.js
@@ -5,21 +5,17 @@ import startCase from 'lodash.startcase'
 
 export default function Card({position, location, compensation, job_type, description, benefits, created_at, updated_at}) {
 
-    console.log(startCase(position))
 
-    // const capitalize = (word) => startCase()
 
     return(
         <>
 
         <JobCard elevation={3}>
-           {/* When on profile we do not want Click event handler */}
            <CardContent >
               <Typography>{startCase(position)}</Typography>
               <Typography>{job_type}</Typography>
               <Typography>${compensation}</Typography>
               <Typography>{startCase(location)}</Typography>
-              
            </CardContent>
         </JobCard>
       
