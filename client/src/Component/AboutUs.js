a583444ef73350b1cf56f489208b6ef79b408ab8
Daniel <danchan020@gmail.com>
2022-07-14 00:33:41 -0700

    nothingworks

--- client/src/Component/AboutUs.js
+++ client/src/Component/AboutUs.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, Avatar, Stack, Card, Typography, Link, CardContent, CardActions, IconButton} from '@mui/material'
+import { Grid, Avatar, Stack, Card, Typography, Link, CardContent, CardActions, CardHeader} from '@mui/material'
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
@@ -9,21 +9,21 @@ export default function AboutUs() {
     <div>
       <Typography 
         align="center" 
-        gutterBottom='true'
+        margin={4}
         variant='h2'
         fontFamily='TimesNewRomanPS-BoldItalicMT'> About Us </Typography>
-      <Stack direction= 'row' spacing={20} justifyContent="center" alignItems="center">
-        <Card variant="outlined" raised={true}>
+      <Stack direction= 'row' spacing={17} justifyContent="center"  marginBottom={5} >
+        <Card variant="outlined" sx={{ width: 365, height: 400 }}>
         <Avatar
-          alt="dchan"
+          alt="Dchan"
           src="/troll.jpeg"
           sx={{ width: 230, height: 230 }}
         />
         <CardContent >
           <Typography align="center" fontFamily='Palatino-Bold' variant='h6'>  Daniel Chan </Typography>
-            <CardActions display="flex" justifyContent="center">
+            <CardActions >
               <Link href="https://github.com/danchan020">
-                <GitHubIcon sx={{ fontSize: "40px" }} style={{color: '#000000'}}/>
+                <GitHubIcon sx={{ fontSize: "40px" }} style={{color: '#000000'}} />
               </Link>
               <Link href="https://www.linkedin.com/in/danielchan-/">
                 <LinkedInIcon sx={{ fontSize: "40px" }}  style={{color: '#0e76a8'}}/>
@@ -31,9 +31,9 @@ export default function AboutUs() {
             </CardActions>
         </CardContent>
         </Card>
-        <Card variant="outlined">
+        <Card variant="outlined" sx={{ width: 365, height: 400 }}>
         <Avatar
-          alt="cstring"
+          alt="Cstring"
           src="/troll3.jpeg"
           sx={{ width: 230, height: 230 }}
         />
@@ -49,9 +49,9 @@ export default function AboutUs() {
           </CardActions>
         </CardContent>
         </Card>
-        <Card variant="outlined">
+        <Card variant="outlined" sx={{ width: 365, height: 400 }}>
         <Avatar
-          alt="knielson"
+          alt="Knielson"
           src="/troll2.png"
           sx={{ width: 230, height: 230 }}
         />
@@ -68,7 +68,7 @@ export default function AboutUs() {
         </CardContent>
         </Card>
       </Stack>
-      <Typography align="center" gutterBottom='true' variant='h6' fontFamily='Palatino-Bold'> 
+      <Typography align="center" marginBottom={5} variant='h6' fontFamily='Palatino-Bold'> 
           Project Repository: &nbsp; <Link href = "https://github.com/knielsen24/fetch"> fetch </Link>
       </Typography>
       
